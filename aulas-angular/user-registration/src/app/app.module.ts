fbf1cf738a8febd280df69b0d793886e9b717e7a
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-05-17 13:04:22 -0300

    Aula inteira

--- aulas-angular/user-registration/src/app/app.module.ts
+++ aulas-angular/user-registration/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListUsersComponent } from './list-users/list-users.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CadastroComponent
-  ],
+  declarations: [AppComponent, CadastroComponent, ListUsersComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
